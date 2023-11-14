a62e88054756689bd567496411f4093f6c9d9db9
indcyr <Indu.Cyriac@ust.com>
2023-11-14 18:14:46 +0530

    14-11-23

---
+++ Assignments/LaunchNewBrowser.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Assignment
+{
+    internal class LaunchNewBrowser
+    {
+        IWebDriver? driver;
+        public void InitializeChromeDriver()
+        {
+
+            driver = new ChromeDriver();      // initializing the chrome driver
+            driver.Url = "https://www.google.com/"; // passing google web page url
+            driver.Navigate().GoToUrl("https://www.yahoo.com/");// going to yahoo web page
+
+            driver.Navigate().Back();// going back to google web page
+
+            driver.Navigate().Forward(); // switching the page to yahoo web page
+
+            driver.Navigate().Back();// going back to google home page
+            Thread.Sleep(3000);
+
+        }
+        public void SearchResultTest()
+        {
+            IWebElement searchInputTextBox = driver.FindElement(By.Id("APjFqb"));// finding the text box by id
+            searchInputTextBox.SendKeys("What's new for diwali 2023"); // passing  what to search
+            Thread.Sleep(3000);
+            IWebElement searchButton = driver.FindElement(By.ClassName("gNO89b")); // selecting the google search button
+            searchButton.Click();  // clicking on the serach button
+            Assert.AreEqual("What's new for diwali 2023 - Google Search", driver.Title); // checking the titles are equal
+            Console.WriteLine("Search test passed");
+            driver.Navigate().Refresh(); // refreshing the page
+
+        }
+        public void Destruct()
+        {
+            driver.Close(); // closing the web page
+        }
+    }
+}
--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -2,18 +2,39 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using Assignments;
+using Assignment;
 
-Amazon amazon = new Amazon();
 
+//14-11-23
+//Assignment 1
+//Amazon amazon = new Amazon();
+
+//try
+//{
+
+
+//amazon.AccessAmazon();
+//    amazon.CheckUrlAmazon();
+//}
+//catch (AssertionException)
+//{
+//    Console.WriteLine("Test - Fail");
+//}
+//amazon.Destruct();
+
+
+//14-11-23
+//Assignment 2
+
+LaunchNewBrowser launchNewBrowser = new LaunchNewBrowser();
 try
 {
+    launchNewBrowser.InitializeChromeDriver(); //calling intialize method
+    launchNewBrowser.SearchResultTest(); // calling SearchResulTest  method
 
-
-amazon.AccessAmazon();
-    amazon.CheckUrlAmazon();
 }
 catch (AssertionException)
 {
-    Console.WriteLine("Test - Fail");
+    Console.WriteLine(" Title Test failed");
 }
-amazon.Destruct();
+launchNewBrowser.Destruct();
